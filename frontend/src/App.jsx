a80b541a286dee953d59f4a2f920c7bb2727c656
AbdulRehman Salman <abdurrehmansalman1234@gmail.com>
2025-07-12 22:47:32 +0500

    cors added and frontend using react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">URL Shortener</h1>
+      <p className="mt-2 text-lg">
+        Transform your long URLs into short, shareable links
+      </p>
+    </div>
+  );
+};
+
+export default App;
